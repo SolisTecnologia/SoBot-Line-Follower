54af188b1f82de72f663605b5743068e52c84e42
Solis Tecnologia - Root <67121369+SolisTecnologia@users.noreply.github.com>
2026-04-23 09:25:33 -0300

    mod: adjust continuous mode parameters and disable MF commands

--- Line_Sensor.py
+++ Line_Sensor.py
@@ -27,7 +27,7 @@ usb.flush()     # Waits data configuration
 usb.write(b"LT E1 RD0 GR50 BL0")    # Turn on led tape in green
 
 sleep(2)
-usb.write(b"MT0 MC AT100 DT100 V2") # Parameter settings for continuous mode
+usb.write(b"MT0 MC MD1 RI50 AT100 DT100 V3") # Parameter settings for continuous mode
 sleep(1)
 usb.write(b"MT0 ME1")               # Enables wheel motors on mode continuous
 sleep(1)
@@ -60,9 +60,9 @@ while flag_enable == 1:
         count_bl = 0
         usb.write(b"LT E1 RD0 GR0 BL50")
         usb.write(b"MT0 ML")        # Turn left
-        sleep(0.6)
-        usb.write(b"MT0 MF")
-        sleep(0.2)
+        sleep(0.4)
+        # usb.write(b"MT0 MF")
+        # sleep(0.1)
 
     # Check if only left sensor is reading black
     elif((left_sensor == black) and (central_sensor == white) and (right_sensor == white)):
@@ -70,9 +70,9 @@ while flag_enable == 1:
         count_bl = 0
         usb.write(b"LT E1 RD0 GR0 BL50")
         usb.write(b"MT0 ML")        # Turn left
-        sleep(0.9)
-        usb.write(b"MT0 MF")
-        sleep(0.2)
+        sleep(0.7)
+        # usb.write(b"MT0 MF")
+        # sleep(0.1)
 
     # Check if central and right sensor is reading black
     elif((left_sensor == white) and (central_sensor == black) and (right_sensor == black)):
@@ -80,9 +80,9 @@ while flag_enable == 1:
         count_bl = 0
         usb.write(b"LT E1 RD0 GR15 BL25")
         usb.write(b"MT0 MR")        # Turn right
-        sleep(0.6)
-        usb.write(b"MT0 MF")
-        sleep(0.2)
+        sleep(0.4)
+        # usb.write(b"MT0 MF")
+        # sleep(0.1)
 
     # Check if only right sensor is reading black
     elif((left_sensor == white) and (central_sensor == white) and (right_sensor == black)):
@@ -90,9 +90,9 @@ while flag_enable == 1:
         count_bl = 0
         usb.write(b"LT E1 RD0 GR15 BL25")
         usb.write(b"MT0 MR")        # Turn right
-        sleep(0.9)
-        usb.write(b"MT0 MF")
-        sleep(0.2)
+        sleep(0.7)
+        # usb.write(b"MT0 MF")
+        # sleep(0.1)
         
     # Check if all sensor is reading white
     elif((left_sensor == white) and (central_sensor == white) and (right_sensor == white)):
